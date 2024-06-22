c23feeaf49d40092e55be6d2897e2086fd0512c3
Rohan Deore <101944321+R0-0NE@users.noreply.github.com>
2024-06-22 10:55:58 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from StudentMLProject.exception import CustomException
 from StudentMLProject.components.data_ingestion import DataIngestion
 from StudentMLProject.components.data_ingestion import DataIngestionConfig
 from src.StudentMLProject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.StudentMLProject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__=="__main__":
     logging.info("The Execution has started")
@@ -21,7 +22,12 @@ if __name__=="__main__":
         
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        
+
+        ##model training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
         
     except Exception as e:
         logging.info("Custom Exception")
